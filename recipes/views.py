1480d483e4568ee323cab6017ba22eaff4f6477b
João Victor da Costa Reis <Joao.costa07@aluno.ifce.edu.br>
2024-11-08 14:52:41 -0300

    Adicionado front end

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
